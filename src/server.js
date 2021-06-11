fee1374c0541d8fa444873e8359bae2aa8da1a41
Walteir Araújo <walteiraraujo@poli.ufrj.br>
2021-06-11 00:51:30 -0300

    API pronta, falta testar

--- src/server.js
+++ src/server.js
@@ -7,8 +7,9 @@ app.use(cors());
 
 app.use(express.json());
 
-const participants = [];
+const participants = [{name: 'walteir', lastStatus: 1623379380731},{name: 'Maria', lastStatus: 1623379380780}];
 const messages = [];
+
 
 app.post('/participants',(req,res)=>{
     if(req.body.name.length===0){
@@ -28,10 +29,85 @@ app.post('/participants',(req,res)=>{
 })
 
 app.get('/participants',(req,res)=>{
-    res.send(participants);
+    //res.send(participants);
+    res.json(participants);
 })
 
+app.post('/messages',(req,res)=>{
+    if(!req.header('User')){
+        res.sendStatus(400);
+        return;
+    }
+    if(!(req.body.to || req.body.text)){
+        res.sendStatus(400);
+        return;
+    }
+    if((req.body.type !== 'message') && (req.body.type !== 'private_message')){
+        res.sendStatus(400);
+        return;
+    }
+    if(participants.find((e)=>e.name===req.header('User'))){
+        res.sendStatus(400);
+        return;
+    }
+    const newMessage = {from: req.header('User'), to: req.body.to, text: req.body.text, type: req.body.type, time: dayjs().format('HH:mm:ss')}
+    messages.push(newMessage);
+    res.sendStatus(200);
+})
+
+app.get('/messages',(req,res)=>{
+    if(!req.header('User')){
+        res.sendStatus(400);
+        return;
+    }
+    const filterMessages = messages.filter((e)=> e.type==='message' || e.to===req.header('User') || e.from===req.header('User') )
+    if(req.query.limit){
+        res.json(filterMessages.slice(-parseInt(limit)));
+        return;
+    }
+    res.json(filterMessages);
+})
+
+app.post('/status',(req,res)=>{
+    if(!req.header('User')){
+        res.sendStatus(400);
+        return;
+    }
+    const participant = participants.find((e)=>e.name===req.header('User'));
+    if(!participant){
+        res.sendStatus(400);
+        return;
+    }
+
+    // const newParticipants = participants.filter((e)=>e.name!==participant.name);
+    // participants = [...newParticipants];
+    // participant.lastStatus=Date.now();
+    // participants=[...newParticipants,participant];
+
+    const index = participants.findIndex((e)=>e.name===req.header('User'));
+    participants[index].lastStatus = Date.now();
+    res.sendStatus(200);
+})
+
+setInterval(()=>{
+    const offlineParticipants = participants.filter((e)=>Date.now()-e.lastStatus>10);
+    offlineParticipants.forEach((e)=>{
+        const offlineMessage = {from: e.name , to: 'Todos', text: 'sai da sala...', type: 'status', time: dayjs().format('HH:mm:ss')}
+        messages.push(offlineMessage);
+    })
+    const onlineParticipants = participants.filter((e)=>Date.now()-e.lastStatus<=10);
+    participants = [...onlineParticipants];
+},15000)
 
 app.listen(4000,()=>{
     console.log('Server is running on port 4000!')
 });
+
+
+// const newParticipants = participants.filter((e)=>e.name!==participant.name);
+//     if(Date.now()-participant.lastStatus>10){
+//         participants = [...newParticipants];
+//     }else{
+//         participant.lastStatus=Date.now();
+//         participants=[...newParticipants,participant];
+//     }
